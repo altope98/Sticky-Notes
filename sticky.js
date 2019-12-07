c1307ef6258a739ce8fe6acb3d200ed0bcc9839c
Álvaro Torrente Pérez <alvarotorrenteperez98@gmail.com>
2019-12-07 16:06:48 +0100

    cambios en visualizacion y boton editar

--- sticky.js
+++ sticky.js
@@ -3,31 +3,51 @@ export class Nota{
         this.title=title;
         this.contenido=contenido;
         this.color=color;
-        this.fecha=new Date();
+        this.f=new Date();
+        this.fecha=this.f.getDate() + "/" + (this.f.getMonth() +1) + "/" + this.f.getFullYear();
         this.not = document.createElement("div");
-        this.not.style.width="150px";
-        this.not.style.height="150px";
+        this.not.style.margin="2rem";
+        /*this.not.style.width="150px";
+        this.not.style.height="150px";*/
         //this.not.setAttribute("height", "40");
         this.not.style.backgroundColor=this.color;
         //this.not.setAttribute("background-color", this.color);
-        
-        
-    }
-
-    crearNota(){
         let h3= document.createElement("h3");
+        h3.style.textAlign="center"
+        h3.style.margin="1rem";
+        h3.style.marginBottom="0";
         h3.textContent= this.title;
         let textarea= document.createElement("textarea");
         textarea.textContent= this.contenido;
+        textarea.style.margin="1rem";
+        textarea.style.marginTop="0.5rem";
+        textarea.style.marginBottom="0";
+        textarea.setAttribute("rows", "10");
+        textarea.setAttribute("cols", "20");
+        textarea.style.backgroundColor=this.color;
+        textarea.style.border="0";
+        let fechas= document.createElement("div");
+        fechas.setAttribute("id","fechas");
+        fechas.style.display="flex";
         let p= document.createElement("p");
         p.textContent= this.fecha;
+        p.style.margin="1rem";
         let transcurrido= document.createElement("p");
+        transcurrido.textContent="-";
+        transcurrido.style.cssFloat="right";
+        fechas.appendChild(p);
+        fechas.appendChild(transcurrido);
+
+        let boton=document.createElement("button");
+        boton.textContent="editar";
+        
+
         this.not.appendChild(h3);
         this.not.appendChild(textarea);
-        this.not.appendChild(p);
-        this.not.appendChild(transcurrido);
-
-
+        this.not.appendChild(fechas);
+        this.not.appendChild(boton);
+        
     }
+
 
 }
--- vista-stickys.js
+++ vista-stickys.js
@@ -2,10 +2,11 @@ import {Nota} from './sticky.js';
 var cont=0;
 
 function mostrarNotas(){
-    var notas=[new Nota("trhhrthr", "rgrge", "red"), new Nota("dvgsdgv", "rthtrhtrh", "yellow")];
+    var notas=[new Nota("trhhrthr", "rgrge", "red"), new Nota("dvgsdgv", "rthtrhtrh", "yellow"), new Nota("vaca","djfsdhjfvbdjf", "blue")];
     var div=document.getElementById("plano");
-    for (const nota of notas) {
-        nota.crearNota();      
+    div.style.display="flex";
+    
+    for (const nota of notas) {   
         nota.not.setAttribute("id", "nota"+cont);
         cont++;
         div.appendChild(nota.not);
@@ -13,3 +14,16 @@ function mostrarNotas(){
 }
 
 mostrarNotas();
+
+
+
+
+//intervalo de tiempo transcurrido cada minuto
+
+//hacer el update con un boton de momento
+
+//hacer las dos vistas al pulsar boton
+
+//cambiar boton por un lapiz o algo
+
+//hacer boton de crear nota y modificar constructor de nota
