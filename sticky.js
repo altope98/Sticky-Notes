784798a32dc52c850dd00de3364d7dc307d2606b
Álvaro Torrente Pérez <alvarotorrenteperez98@gmail.com>
2019-12-12 01:14:19 +0100

    falta mostrar desde localstorage, hacer vista tabla, cambiar botones y css

--- sticky.js
+++ sticky.js
@@ -1,53 +1,62 @@
+var cont;
+
 export class Nota{
-    constructor(title, contenido, color){
+    constructor(title){
+        this.content="";
         this.title=title;
-        this.contenido=contenido;
-        this.color=color;
         this.f=new Date();
         this.fecha=this.f.getDate() + "/" + (this.f.getMonth() +1) + "/" + this.f.getFullYear();
         this.not = document.createElement("div");
         this.not.style.margin="2rem";
-        /*this.not.style.width="150px";
-        this.not.style.height="150px";*/
-        //this.not.setAttribute("height", "40");
-        this.not.style.backgroundColor=this.color;
-        //this.not.setAttribute("background-color", this.color);
-        let h3= document.createElement("h3");
-        h3.style.textAlign="center"
-        h3.style.margin="1rem";
-        h3.style.marginBottom="0";
-        h3.textContent= this.title;
-        let textarea= document.createElement("textarea");
-        textarea.textContent= this.contenido;
-        textarea.style.margin="1rem";
-        textarea.style.marginTop="0.5rem";
-        textarea.style.marginBottom="0";
-        textarea.setAttribute("rows", "10");
-        textarea.setAttribute("cols", "20");
-        textarea.style.backgroundColor=this.color;
-        textarea.style.border="0";
-        let fechas= document.createElement("div");
-        fechas.setAttribute("id","fechas");
-        fechas.style.display="flex";
-        let p= document.createElement("p");
-        p.textContent= this.fecha;
-        p.style.margin="1rem";
-        let transcurrido= document.createElement("p");
-        transcurrido.textContent="-";
-        transcurrido.style.cssFloat="right";
-        fechas.appendChild(p);
-        fechas.appendChild(transcurrido);
+        this.not.style.backgroundColor="yellow";
 
-        let boton=document.createElement("button");
-        boton.textContent="editar";
+        this.botoneliminar=document.createElement("button");
+        this.botoneliminar.setAttribute("id", "eliminar"+cont);
         
+        this.botoneliminar.textContent="eliminar";
 
-        this.not.appendChild(h3);
-        this.not.appendChild(textarea);
-        this.not.appendChild(fechas);
-        this.not.appendChild(boton);
+        this.h3= document.createElement("h3");
+        this.h3.style.textAlign="center"
+        this.h3.style.margin="1rem";
+        this.h3.style.marginBottom="0";
+        this.h3.textContent= this.title;
+        this.textarea= document.createElement("textarea");
+        this.textarea.textContent= "...";
+        this.textarea.style.margin="1rem";
+        this.textarea.style.marginTop="0.5rem";
+        this.textarea.style.marginBottom="0";
+        this.textarea.setAttribute("rows", "10");
+        this.textarea.setAttribute("cols", "20");
+        this.textarea.style.backgroundColor="yellow";
+        this.textarea.style.border="0";
+        this.fechas= document.createElement("div");
+        this.fechas.setAttribute("id","fechas");
+        this.fechas.style.display="flex";
+        this.p= document.createElement("p");
+        this.p.textContent= this.fecha;
+        this.p.style.margin="1rem";
+        this.transcurrido= document.createElement("p");
+        this.transcurrido.textContent="-";
+        this.transcurrido.style.cssFloat="right";
+
+        this.not.setAttribute("id", "nota"+cont);
+        
+        this.fechas.appendChild(this.p);
+        this.fechas.appendChild(this.transcurrido);
+
+        this.boton=document.createElement("button");
+        this.boton.setAttribute("id", "editar"+cont);
+        
+        this.boton.textContent="editar";
+        
+        this.not.appendChild(this.botoneliminar);
+        this.not.appendChild(this.h3);
+        this.not.appendChild(this.textarea);
+        this.not.appendChild(this.fechas);
+        this.not.appendChild(this.boton);
+
+        cont++;
         
     }
-
 
 }
--- vista-stickys.js
+++ vista-stickys.js
@@ -1,29 +1,117 @@
 import {Nota} from './sticky.js';
 var cont=0;
+var notas=[];
 
-function mostrarNotas(){
-    var notas=[new Nota("trhhrthr", "rgrge", "red"), new Nota("dvgsdgv", "rthtrhtrh", "yellow"), new Nota("vaca","djfsdhjfvbdjf", "blue")];
-    var div=document.getElementById("plano");
-    div.style.display="flex";
-    
-    for (const nota of notas) {   
-        nota.not.setAttribute("id", "nota"+cont);
-        cont++;
-        div.appendChild(nota.not);
-    }
+window.onload = () => {
+    mostrarTablon();
 }
 
-mostrarNotas();
+function mostrarTablon() {
+    var div=document.getElementById("plano");
+    document.body.style.backgroundImage="url('https://www.publicdomainpictures.net/pictures/30000/velka/cork-board.jpg')";
+    div.style.display="flex";
+    mostrarNotas();
+
+    let div2=document.createElement("div");
+    
+    
+    let botonNueva=document.createElement("button");
+    botonNueva.setAttribute("id","nueva");
+    botonNueva.addEventListener("click", crearNota);
+    botonNueva.textContent="Nueva nota";
+
+    let input=document.createElement("input");
+    input.setAttribute("id","input");
+    input.value="Escribe titulo aqui...";
 
 
+    div2.appendChild(input);
+    div2.appendChild(botonNueva);
+
+    let botonCambio=document.createElement("button");
+    botonCambio.setAttribute("id","cambio");
+    botonCambio.textContent="Cambiar apariencia";
+    div2.appendChild(botonCambio);
+    div.appendChild(div2);
 
 
-//intervalo de tiempo transcurrido cada minuto
+}
 
-//hacer el update con un boton de momento
+function mostrarNotas(){
+    var temporizador=1;
+    let div=document.getElementById("plano");
+    setInterval(() => {
+        
+        for (const nota of notas) {
+            if(nota!=null){
+                div.appendChild(nota.not);
+            if(nota.transcurrido.textContent=="-"){
+                nota.transcurrido.textContent=temporizador+" minuto";
+            }else{
+                temporizador++;
+                nota.transcurrido.textContent=temporizador+" minutos";
+            }
+        }
+        }
+    }, 60000);
+    
+    console.log(notas);
+    for (const nota of notas) {   
+        if(nota!=null){
 
-//hacer las dos vistas al pulsar boton
+            nota.boton.addEventListener("click", actualizar);
+            nota.botoneliminar.addEventListener("click", eliminar);
+            div.appendChild(nota.not);
 
-//cambiar boton por un lapiz o algo
+        }
 
-//hacer boton de crear nota y modificar constructor de nota
+    }
+
+    /*let parse=localStorage.getItem("notas");
+    notas=JSON.parse(parse);*/
+}
+
+function crearNota() {
+    let input=document.getElementById("input");
+    notas.push(new Nota(input.value));
+    mostrarNotas();
+    localStorage.setItem("notas", JSON.stringify(notas));
+
+    
+}
+
+function actualizar() {
+    let div=document.getElementById("plano");
+    for (let i = 0; i < notas.length; i++) {
+        let nota=notas[i];
+        if(event.target.id==nota.boton.id){
+            nota.content=nota.textarea.value;
+            notas[i]=null;
+            notas[i]=nota;
+        }     
+    }
+    mostrarNotas();
+    localStorage.setItem("notas", JSON.stringify(notas));
+}
+
+function eliminar() {
+    let div=document.getElementById("plano");
+    for (let i = 0; i < notas.length; i++) {
+        let nota=notas[i];
+        if(event.target.id==nota.botoneliminar.id){
+            notas[i]=null;
+            div.removeChild(nota.not);
+        }
+    }
+    mostrarNotas();
+    localStorage.setItem("notas", JSON.stringify(notas));
+}
+
+
+//HACER LA OTRA VISTA (DE TABLA) AL PULSAR BOTON
+
+//CONSEGUIR MOSTRAR DESDE LOCALSTORAGE LAS NOTAS
+
+//CAMBIAR BOTON POR UN LAPIZ Y EL DE CERRAR POR UNA X
+
+//MAQUEAR TODO UN POCO
